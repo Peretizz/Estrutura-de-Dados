66c0e1a7941fc27ae78c954d3a5ac2239a6db36b
Nicolas Pereti <282864@unoesc.lan>
2026-08-05 19:48:16 -0300

    Adicionando arquivos de Estrutura de Dados

--- Vetores/vetores/obj/Debug/net10.0/vetores.AssemblyInfo.cs
+++ Vetores/vetores/obj/Debug/net10.0/vetores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vetores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad1e949f866bc30ed833505a2e2c9a14f8690e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("vetores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vetores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
