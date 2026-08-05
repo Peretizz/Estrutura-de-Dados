0ad1e949f866bc30ed833505a2e2c9a14f8690e3
Nicolas Pereti <282864@unoesc.lan>
2026-08-05 19:43:33 -0300

    Primeiro commit do projeto local

---
+++ Vetores/vetores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Serialization;
+
+Console.WriteLine("Hello, World!");
+
+//comentário de uma linha
+
+/*
+Comentário de
+múltiplas linhas
+*/
+
+//Alguns tipos de dados
+int number = 10;
+string name = "Nicolas Pereti";
+double quantity = 0.0;
+bool hasMoney = true;
+char letter = 'A';
+
+// Constantes
+const int DAYS_IN_WEEK = 7;
+
+letter = 'B';
+
+Console.WriteLine ("Informe seu nome:");
+name = Console.ReadLine ();
+
+Console.WriteLine("Seja bem-vindo " + name);
+
+//Enumeradores
+//enum Language { PT, EN, ES }
+
+//Concatenando strings
+
+string firstName = "Nicolas", lastname = "";
+lastname = "Pereti";
+
+string fullName = firstName + " " + lastname;
+Console.WriteLine(fullName);
+
+string middleName = "Negrao";
+
+//Interpolação de string
+string message = $"Nome Completo: {firstName} {middleName} {lastname}";
+Console.WriteLine(message);
