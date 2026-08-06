214b909b6eb38797ac99e7310893e9023ed7d85e
Peretizz <peretinicolas82@gmail.com>
2026-08-05 22:03:20 -0300

    Aula 05/08

---
+++ Palavras/Program.cs
@@ -0,0 +1,13 @@
+﻿string firstName = "Gabriel";
+string lastName = "Jesus";
+
+string note = lastName.ToUpper () + " " + firstName;
+
+string initials = lastName[0] + "" + firstName[0];
+
+Console.WriteLine("" + initials);
+
+// Formatação de String
+string texto = string.Format ("{0} {1} nascido em {2}", firstName, lastName, "2009");
+
+Console.WriteLine (texto);
