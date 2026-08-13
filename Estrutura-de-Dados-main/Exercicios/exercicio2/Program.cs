c3624985c06bfac29bcaac7e18ecb5f4a90666c0
Peretizz <peretinicolas82@gmail.com>
2026-08-12 21:39:14 -0300

    Exercicio 2

---
+++ Estrutura-de-Dados-main/Exercicios/exercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿string palavra = "";
+
+Console.WriteLine("Digite uma palavra:");
+palavra = Console.ReadLine();
+
+string[] letras = new string[palavra.Length];
+string[] letras2 = new string[palavra.Length];
+
+for (int i = 0; i < palavra.Length; i++)
+{
+    letras[i] = palavra[i].ToString();
+}
+
+for (int i = palavra.Length - 1; i >= 0; i--)
+{
+    letras2[i] = palavra[palavra.Length - 1 - i].ToString();
+}
+
+bool palindromo = true;
+
+for (int i = 0; i < palavra.Length; i++)
+{
+    if (letras[i] != letras2[i])
+    {
+        palindromo = false;
+        break;
+    }
+}
+
+if (palindromo)
+{
+    Console.WriteLine("A palavra é um palíndromo");
+}
+else
+{
+    Console.WriteLine("A palavra não é um palíndromo");
+}
