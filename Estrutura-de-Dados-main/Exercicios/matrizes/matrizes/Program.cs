05386287a3f6b6e355ebdc062b08a900ff0425ca
Peretizz <peretinicolas82@gmail.com>
2026-08-19 21:37:20 -0300

    Matrizes

--- Estrutura-de-Dados-main/Exercicios/matrizes/matrizes/Program.cs
+++ Estrutura-de-Dados-main/Exercicios/matrizes/matrizes/Program.cs
@@ -25,3 +25,30 @@ for (int i = 0; i < newNumbersBi.GetLength(0); i++)
     }
     Console.WriteLine("");
 }
+
+// Jagged Arrays ou Arréi de Jegues
+// Vetor de Vetores 
+
+int[][] jaggedNumbers = new int[4][];
+
+// Atribuindo valores 
+jaggedNumbers[0] = new int[] { 9, 5, -9 };
+jaggedNumbers[1] = new int[] { 0, -3, 12, 51, -3 };
+jaggedNumbers[3] = new int[] { 51 }; 
+
+
+// Declaração com inicialização
+int[][] newJaggedNumbers2 = new int[][]
+{
+    new int[] { 0, 1 , 2 },
+    new int[] { 3, 4, 5, 6 },
+    null,
+    new int[] { 7, 8, 9, 10 },
+};
+
+// Para acessar uma posição específica 
+int v = jaggedNumbers[1][2];
+jaggedNumbers[1][2] = 22;
+
+Console.WriteLine("v = " + v);
+Console.WriteLine("JaggedNumbers[1][2] = " + jaggedNumbers[1][2]);
