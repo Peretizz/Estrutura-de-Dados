c6b06611041b08f184cbc25ce517d51d7f168625
Peretizz <peretinicolas82@gmail.com>
2026-09-23 20:42:53 -0300

    aula 23/09

---
+++ Estrutura-de-Dados-main/stacks/stack/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+Stack<char> chars = new Stack<char>();
+
+string text = string.Empty;
+Console.WriteLine("Digite uma palavra: ");
+text = Console.ReadLine();
+
+string reversedText = string.Empty;
+foreach (char c in text)
+{
+    chars.Push(c);
+}
+while (chars.Count > 0)
+{
+    reversedText += chars.Pop();
+}
+Console.WriteLine(reversedText);
+
+string isPalindromo = text == reversedText ? "É um palíndromo" : "Não é um palíndromo";
